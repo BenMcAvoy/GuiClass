354ea9c71aa652a11dc11b7409cc45a0d4e0fb3e
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-04-15 00:51:03 +0100

    feat: create add/sub memory buttons

--- src/Ribbon.h
+++ src/Ribbon.h
@@ -13,7 +13,37 @@ void Ribbon::Render() {
 	ImGui::SetNextWindowSize(ImVec2(ImGui::GetIO().DisplaySize.x, 96), ImGuiCond_Always);
 	ImGui::Begin("Ribbon", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
 
-	ImGui::Text("Ribbon");
+	ImGui::Columns(3, "ribbon", false);
+	ImGui::SetColumnWidth(0, 96);
+	ImGui::SetColumnWidth(1, 96);
+
+	const ImVec2 size = ImVec2(96, 24);
+
+	if (ImGui::Button("Add 4", size)) {
+		INFO("Add 4 callback\n");
+	};
+
+	if (ImGui::Button("Add 16", size)) {
+		INFO("Add 16 callback\n");
+	};
+
+	if (ImGui::Button("Add 256", size)) {
+		INFO("Add 256 callback\n");
+	};
+
+	ImGui::NextColumn();
+
+	if (ImGui::Button("Sub 4", size)) {
+		INFO("Subtract 4 callback\n");
+	};
+
+	if (ImGui::Button("Sub 16", size)) {
+		INFO("Subtract 16 callback\n");
+	};
+
+	if (ImGui::Button("Sub 256", size)) {
+		INFO("Subtract 256 callback\n");
+	};
 
 	ImGui::End();
 }
