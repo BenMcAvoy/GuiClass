c329bb9ba9581d7089a22699ba1c15dcf62cb118
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-04-15 17:48:39 +0100

    fix: set window title

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int main(void) {
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "GuiClass", NULL, NULL);
 
 	if (!window) {
 		glfwTerminate();
